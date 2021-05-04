30e9481a63d913dc3b35171d4cf1a59e0564646e
Helder Burato Berto <helder.burato@gmail.com>
2021-05-04 11:59:43 +0100

    Remove unnecesary SEO tags from home

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,5 @@
 import { getAllFilesFrontMatter } from '@/lib/mdx'
-import siteMetadata from '@/data/siteMetadata'
 import ListLayout from '@/layouts/ListLayout'
-import { PageSeo } from '@/components/SEO'
 
 export async function getStaticProps() {
   const posts = await getAllFilesFrontMatter('blog')
@@ -10,14 +8,5 @@ export async function getStaticProps() {
 }
 
 export default function Blog({ posts }) {
-  return (
-    <>
-      <PageSeo
-        title={`Blog - ${siteMetadata.author}`}
-        description={siteMetadata.description}
-        url={`${siteMetadata.siteUrl}`}
-      />
-      <ListLayout posts={posts} />
-    </>
-  )
+  return <ListLayout posts={posts} />
 }
