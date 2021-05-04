5266e7c328748ebf2605aab383ad375ec785ce1b
Helder Burato Berto <helder.burato@gmail.com>
2021-05-04 14:09:25 +0100

    Fix home title

--- pages/index.js
+++ pages/index.js
@@ -13,11 +13,7 @@ export async function getStaticProps() {
 export default function Home({ posts }) {
   return (
     <>
-      <PageSeo
-        title={siteMetadata.title}
-        description={siteMetadata.description}
-        url={siteMetadata.siteUrl}
-      />
+      <PageSeo title="Home" description={siteMetadata.description} url={siteMetadata.siteUrl} />
       <div className="pt-6 pb-8 space-y-2 md:space-y-5">
         <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
           {siteMetadata.description}
