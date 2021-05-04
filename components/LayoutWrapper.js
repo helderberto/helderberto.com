a328cda37a846e06d8af57d1de813351f139c973
Helder Burato Berto <helder.burato@gmail.com>
2021-05-04 14:22:40 +0100

    Fix logo on mobile view

--- components/LayoutWrapper.js
+++ components/LayoutWrapper.js
@@ -12,15 +12,11 @@ const LayoutWrapper = ({ children }) => {
       <div className="flex flex-col justify-between h-screen">
         <header className="flex items-center justify-between py-10">
           <div>
-            <Link href="/" aria-label="Tailwind CSS Blog">
+            <Link href="/" aria-label="Helder Burato Berto">
               <div className="flex items-center justify-between">
-                {typeof siteMetadata.headerTitle === 'string' ? (
-                  <div className="hidden h-6 text-2xl font-semibold sm:block">
-                    {siteMetadata.headerTitle}
-                  </div>
-                ) : (
-                  siteMetadata.headerTitle
-                )}
+                <div className="h-6 text-2xl font-semibold sm:block">
+                  {siteMetadata.headerTitle}
+                </div>
               </div>
             </Link>
           </div>
