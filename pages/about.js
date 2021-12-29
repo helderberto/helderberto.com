1e7af081de781eaf48d21fa3aa10a1ca9fe4b004
Helder Burato Berto <helder.burato@gmail.com>
2021-12-29 11:20:54 +0000

    Change "about" to "about me"

--- pages/about.js
+++ pages/about.js
@@ -6,14 +6,14 @@ export default function About() {
   return (
     <>
       <PageSeo
-        title={`About - ${siteMetadata.author}`}
+        title="About me"
         description={`About me - ${siteMetadata.author}`}
         url={`${siteMetadata.siteUrl}/about`}
       />
       <div className="divide-y">
         <div className="pt-6 pb-8 space-y-2 md:space-y-5">
           <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
-            About
+            About me
           </h1>
         </div>
         <div className="items-start space-y-2 xl:grid xl:grid-cols-3 xl:gap-x-8 xl:space-y-0">
