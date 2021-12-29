77856b9057ca5a248e1c7ccc8ec7a9ef181e4bf6
Helder Burato Berto <helder.burato@gmail.com>
2021-12-29 12:00:34 +0000

    Update personal description

--- pages/index.js
+++ pages/index.js
@@ -14,8 +14,15 @@ export default function Home({ posts }) {
   return (
     <>
       <PageSeo title="Home" description={siteMetadata.description} url={siteMetadata.siteUrl} />
-      <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
-        {siteMetadata.description}
+      <p className="text-lg leading-7 text-gray-500 dark:text-gray-400 ">
+        Hey! I'm{' '}
+        <a
+          href="https://twitter.com/helderburato"
+          className="text-green-500 hover:text-green-600 dark:hover:text-green-400"
+        >
+          Helder Burato Berto
+        </a>
+        . A Brazilian Front End Developer living in Porto, Portugal.
       </p>
       <ListLayout posts={posts} />
     </>
