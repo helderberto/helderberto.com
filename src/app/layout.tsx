26002b66bd6bdfbd6d9f448015f65f5c6a2d9381
Helder Burato Berto <helder.burato@gmail.com>
2024-11-05 10:11:08 +0000

    Refactor package.json to add @vercel/speed-insights dependency

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import Header from "@/components/Header";
 import { ThemeProvider } from "@/components/ThemeProvider";
 import { siteConfig } from "@/config/site";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
@@ -100,6 +101,7 @@ export default function RootLayout({
           <main className={styles.main}>{children}</main>
         </ThemeProvider>
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
