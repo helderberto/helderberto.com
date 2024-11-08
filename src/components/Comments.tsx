7e0a8799384644c1872f3fc6a7121ddda0eb31b1
Helder Burato Berto <helder.burato@gmail.com>
2024-11-08 15:57:26 +0000

    Refactor CodeBlock component to remove Comments import and update CSS class names

--- src/components/Comments.tsx
+++ src/components/Comments.tsx
@@ -6,35 +6,43 @@ import { useEffect, useRef } from "react";
 import styles from "./Comments.module.css";
 
 export const Comments = () => {
-  const commentRef = useRef<HTMLDivElement>(null);
-  const { theme, systemTheme } = useTheme();
-
-  const currentTheme = theme === "system" ? systemTheme : theme;
-  const utterancesTheme =
-    currentTheme === "dark" ? "github-dark" : "github-light";
+  const commentBox = useRef<HTMLDivElement>(null);
+  const { theme } = useTheme();
 
   useEffect(() => {
-    const scriptParentElement = commentRef.current;
-    const utterancesEl = scriptParentElement?.querySelector(".utterances");
-    if (utterancesEl) {
-      utterancesEl.remove();
+    const commentsTheme = theme === "dark" ? "github-dark" : "github-light";
+
+    const commentScript = document.createElement("script");
+    commentScript.async = true;
+    commentScript.src = "https://utteranc.es/client.js";
+    // define the name of the repository you created here as 'owner/repo'
+    // or import it from your config file if you have one.
+    commentScript.setAttribute("repo", siteConfig.comments.repo);
+    // define the blog post -> issue mapping (e.g. page pathname, page url).
+    // here the issues will be created with the page pathname as the issue title.
+    commentScript.setAttribute("issue-term", "pathname");
+    // define a custom label that you want added to your posts.
+    commentScript.setAttribute("label", siteConfig.comments.label);
+    // define if you want to use dark or light theme.
+    commentScript.setAttribute("theme", commentsTheme);
+    commentScript.setAttribute("crossorigin", "anonymous");
+
+    // we will append this script as a child to the ref element we have created above
+    if (commentBox && commentBox.current) {
+      commentBox.current.appendChild(commentScript);
+    } else {
+      console.log(`Error adding utterances comments on: ${commentBox}`);
     }
 
-    const script = document.createElement("script");
-    script.setAttribute("src", "https://utteranc.es/client.js");
-    script.setAttribute("repo", siteConfig.comments.repo);
-    script.setAttribute("crossorigin", "anonymous");
-    script.setAttribute("issue-term", "url");
-    script.setAttribute("theme", utterancesTheme);
-    script.setAttribute("label", siteConfig.comments.label);
-    script.setAttribute("async", "true");
-
-    commentRef.current?.appendChild(script);
-  }, [utterancesTheme]);
+    return () => {
+      commentScript.remove();
+      document.querySelectorAll(".utterances").forEach((el) => el.remove());
+    };
+  }, [theme]);
 
   return (
     <div className={styles.comments}>
-      <div ref={commentRef} />
+      <div ref={commentBox} />
     </div>
   );
 };
