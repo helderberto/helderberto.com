5242e15d7c0af3723ee1bc7a7353686e04c65e47
Helder Burato Berto <helder.burato@gmail.com>
2026-08-21 20:19:55 +0100

    feat: add RSS feed with autodiscovery and footer/palette links (#5)

---
+++ src/app/feed.xml/route.ts
@@ -0,0 +1,50 @@
+import { siteConfig } from '@/config/site';
+import { getAllPosts } from '@/lib/posts';
+
+export const dynamic = 'force-static';
+
+const escapeXml = (value: string) =>
+  value
+    .replaceAll('&', '&amp;')
+    .replaceAll('<', '&lt;')
+    .replaceAll('>', '&gt;')
+    .replaceAll('"', '&quot;')
+    .replaceAll("'", '&apos;');
+
+export async function GET() {
+  const posts = await getAllPosts();
+
+  const items = posts
+    .map((post) => {
+      const url = `${siteConfig.url}/posts/${post.slug}`;
+      return `    <item>
+      <title>${escapeXml(post.title)}</title>
+      <link>${url}</link>
+      <guid isPermaLink="true">${url}</guid>
+      <pubDate>${new Date(post.date).toUTCString()}</pubDate>
+      <description>${escapeXml(post.excerpt)}</description>
+    </item>`;
+    })
+    .join('\n');
+
+  const lastBuildDate = new Date(posts[0]?.date ?? Date.now()).toUTCString();
+
+  const xml = `<?xml version="1.0" encoding="UTF-8"?>
+<rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
+  <channel>
+    <title>${escapeXml(siteConfig.name)}</title>
+    <link>${siteConfig.url}</link>
+    <description>${escapeXml(siteConfig.description)}</description>
+    <language>en-us</language>
+    <lastBuildDate>${lastBuildDate}</lastBuildDate>
+    <atom:link href="${siteConfig.url}/feed.xml" rel="self" type="application/rss+xml"/>
+${items}
+  </channel>
+</rss>`;
+
+  return new Response(xml, {
+    headers: {
+      'Content-Type': 'application/rss+xml; charset=utf-8',
+    },
+  });
+}
